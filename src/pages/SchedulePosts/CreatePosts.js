9dc12e0ba7568013a57fdac3e0d0133b6ada8cca
Shayan Zulfiqar <146065078+ShayanZulfiqar@users.noreply.github.com>
2024-09-13 21:43:35 +0400

    Shayan || Update

--- src/pages/SchedulePosts/CreatePosts.js
+++ src/pages/SchedulePosts/CreatePosts.js
@@ -34,6 +34,28 @@ const CreatePosts = () => {
                     <p>Select a social account and a post to preview</p>
                   </div>
                 </div>
+                <div className="post-card">
+    <div className="post-header">
+        <img src="profile-pic.png" alt="Profile Picture" className="profile-pic" />
+        <div>
+            <h5 className="mb-0">Company</h5>
+            <small className="text-muted">Just now</small>
+        </div>
+        <div className="ml-auto">
+            <i className="fas fa-ellipsis-h"></i>
+        </div>
+    </div>
+    <div className="post-content mt-3">
+        <p>This is an awesome post made using Bootstrap and Font Awesome icons!</p>
+    </div>
+    <div className="post-actions">
+        <a href="#"><i className="far fa-thumbs-up"></i> Like</a>
+        <a href="#"><i className="far fa-comment"></i> Comment</a>
+        <a href="#"><i className="fas fa-share"></i> Share</a>
+    </div>
+</div>
+
+                
               </div>
             </div>
           </div>
